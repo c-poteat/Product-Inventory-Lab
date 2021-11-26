741b193d328f59ae71cfcc9440315a9a277ad54e
carnell <carnell@Skynet.hsd1.de.comcast.net>
2021-11-25 20:02:22 -0500

    updated lab

--- src/main/java/models/BarFood.java
+++ src/main/java/models/BarFood.java
@@ -1,4 +1,29 @@
 package models;
 
 public class BarFood {
+
+    private int numberOfGlasses;
+    private int numberOfPlates;
+    private int numberOfSilverWare;
+    private String Burgers;
+    private String Sandwiches;
+    private String ChickenBreast;
+    private int qty;
+    private float price;
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
